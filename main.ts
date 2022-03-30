8c9a44e632ca2437beb840cbfd3d25a76e3832ff
anonimo70p <98742584+anonimo70p@users.noreply.github.com>
2022-03-30 09:27:50 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(12345)
+})
+basic.showString("¡hola¡¿Como estas?")
+music.playMelody("C5 G E C F C B A ", 60)
+basic.forever(function () {
+	
+})
